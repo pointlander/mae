ac1f1df37ba46fa104746240b2acdfcec38c5720
Andrew Snodgrass <pointlander@gmail.com>
2024-11-30 16:07:41 -0600

    Refactor

--- main.go
+++ main.go
@@ -50,22 +50,39 @@ const (
 	StateTotal
 )
 
-func main() {
-	data := Load()
-	rng := rand.New(rand.NewSource(1))
+// Loss is a network loss
+type Loss struct {
+	Network int
+	Loss    float64
+}
 
-	type Network struct {
-		Set    tf64.Set
-		Others tf64.Set
-		L1     tf64.Meta
-		L2     tf64.Meta
-		Loss   tf64.Meta
-		I      int
-		V      tf64.Meta
-		VV     tf64.Meta
-		E      tf64.Meta
-	}
-	networks := make([]Network, Networks)
+// Network is a autoencoding neural network
+type Network struct {
+	Set    tf64.Set
+	Others tf64.Set
+	L1     tf64.Meta
+	L2     tf64.Meta
+	Loss   tf64.Meta
+	I      int
+	V      tf64.Meta
+	VV     tf64.Meta
+	E      tf64.Meta
+}
+
+// System is a system of autoencoding neural networks
+type System struct {
+	Rng       *rand.Rand
+	Networks  []Network
+	Subsets   int
+	Width     int
+	Embedding int
+	I         int
+}
+
+// NewSystem makes a new system
+func NewSystem(seed int64, nets, subsets, width, embedding int) System {
+	rng := rand.New(rand.NewSource(1))
+	networks := make([]Network, nets)
 	/*drop := 0.9
 	dropout := map[string]interface{}{
 		"rng":  rng,
@@ -73,10 +90,10 @@ func main() {
 	}*/
 	for n := range networks {
 		set := tf64.NewSet()
-		set.Add("w1", Embedding, Width/2)
-		set.Add("b1", Width/2)
-		set.Add("w2", Width/2, Width)
-		set.Add("b2", Width)
+		set.Add("w1", embedding, width/2)
+		set.Add("b1", width/2)
+		set.Add("w2", width/2, width)
+		set.Add("b2", width)
 
 		for i := range set.Weights {
 			w := set.Weights[i]
@@ -99,8 +116,8 @@ func main() {
 		}
 
 		others := tf64.NewSet()
-		others.Add("input", Embedding, Batch)
-		others.Add("output", Width, Batch)
+		others.Add("input", embedding, Batch)
+		others.Add("output", width, Batch)
 
 		for i := range others.Weights {
 			w := others.Weights[i]
@@ -123,100 +140,111 @@ func main() {
 		networks[n].VV = vv
 		networks[n].E = e
 	}
-
-	pow := func(x float64, i int) float64 {
-		y := math.Pow(x, float64(i+1))
-		if math.IsNaN(y) || math.IsInf(y, 0) {
-			return 0
-		}
-		return y
+	return System{
+		Rng:       rng,
+		Networks:  networks,
+		Subsets:   subsets,
+		Width:     width,
+		Embedding: embedding,
+		I:         0,
 	}
+}
 
-	type Loss struct {
-		Network int
-		Loss    float64
+// Pow returns the input raised to the current time
+func Pow(x float64, i int) float64 {
+	y := math.Pow(x, float64(i+1))
+	if math.IsNaN(y) || math.IsInf(y, 0) {
+		return 0
 	}
+	return y
+}
 
-	points := make(plotter.XYs, 0, 8)
-	for i := 0; i < 4*33*len(data); i++ {
-		factor := Factor*math.Sin(2*math.Pi*float64(i)/256.0) + Factor + 0.1
-		eta := Eta*math.Sin(2*math.Pi*float64(2*i)/256.0) + Eta
-		index := rng.Intn(len(data))
-		for s := 0; s < Batch; s++ {
-			transform := MakeRandomTransform(rng, Width, Embedding, factor)
-			//offset := MakeRandomTransform(rng, Width, 1, Scale)
-			in := NewMatrix(Width, 1, data[index].Measures...)
-			in = transform.MulT(in) //.Add(offset).Softmax()
-			for n := range networks {
-				copy(networks[n].Others.ByName["input"].X[s*Embedding:(s+1)*Embedding], in.Data)
-				copy(networks[n].Others.ByName["output"].X[s*Width:(s+1)*Width], data[index].Measures)
-			}
+// Learn learns a vector
+func (s *System) Learn(vector []float64) float64 {
+	cost := 0.0
+	factor := Factor*math.Sin(2*math.Pi*float64(s.I)/256.0) + Factor + 0.1
+	eta := Eta*math.Sin(2*math.Pi*float64(2*s.I)/256.0) + Eta
+	networks := s.Networks
+	for b := 0; b < Batch; b++ {
+		transform := MakeRandomTransform(s.Rng, s.Width, s.Embedding, factor)
+		//offset := MakeRandomTransform(rng, Width, 1, Scale)
+		in := NewMatrix(Width, 1, vector...)
+		in = transform.MulT(in) //.Add(offset).Softmax()
+		for n := range s.Networks {
+			copy(networks[n].Others.ByName["input"].X[b*s.Embedding:(b+1)*s.Embedding], in.Data)
+			copy(networks[n].Others.ByName["output"].X[b*s.Width:(b+1)*s.Width], vector)
 		}
-		losses := make([]Loss, Networks)
-		for n := range networks {
-			networks[n].Others.Zero()
-			networks[n].VV(func(a *tf64.V) bool {
-				losses[n].Network = n
-				losses[n].Loss = a.X[0]
-				return true
-			})
-		}
-
-		sort.Slice(losses, func(i, j int) bool {
-			return losses[i].Loss < losses[j].Loss
+	}
+	losses := make([]Loss, Networks)
+	for n := range networks {
+		networks[n].Others.Zero()
+		networks[n].VV(func(a *tf64.V) bool {
+			losses[n].Network = n
+			losses[n].Loss = a.X[0]
+			return true
 		})
-
-		for _, loss := range losses[:3] {
-			network := loss.Network
-			networks[network].Others.Zero()
-			networks[network].Set.Zero()
-			cost := tf64.Gradient(networks[network].Loss).X[0]
-
-			norm := 0.0
-			for _, p := range networks[network].Set.Weights {
-				for _, d := range p.D {
-					norm += d * d
-				}
-			}
-			norm = math.Sqrt(norm)
-			b1, b2 := pow(B1, networks[network].I), pow(B2, networks[network].I)
-			scaling := 1.0
-			if norm > 1 {
-				scaling = 1 / norm
-			}
-			for _, w := range networks[network].Set.Weights {
-				for l, d := range w.D {
-					g := d * scaling
-					m := B1*w.States[StateM][l] + (1-B1)*g
-					v := B2*w.States[StateV][l] + (1-B2)*g*g
-					w.States[StateM][l] = m
-					w.States[StateV][l] = v
-					mhat := m / (1 - b1)
-					vhat := v / (1 - b2)
-					if vhat < 0 {
-						vhat = 0
-					}
-					w.X[l] -= eta * mhat / (math.Sqrt(vhat) + 1e-8)
-				}
-			}
-
-			points = append(points, plotter.XY{X: float64(i), Y: float64(cost)})
-			networks[network].I++
-		}
 	}
 
+	sort.Slice(losses, func(i, j int) bool {
+		return losses[i].Loss < losses[j].Loss
+	})
+
+	for _, loss := range losses[:s.Subsets] {
+		network := loss.Network
+		networks[network].Others.Zero()
+		networks[network].Set.Zero()
+		cost += tf64.Gradient(networks[network].Loss).X[0]
+
+		norm := 0.0
+		for _, p := range networks[network].Set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = math.Sqrt(norm)
+		b1, b2 := Pow(B1, networks[network].I), Pow(B2, networks[network].I)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+		for _, w := range networks[network].Set.Weights {
+			for l, d := range w.D {
+				g := d * scaling
+				m := B1*w.States[StateM][l] + (1-B1)*g
+				v := B2*w.States[StateV][l] + (1-B2)*g*g
+				w.States[StateM][l] = m
+				w.States[StateV][l] = v
+				mhat := m / (1 - b1)
+				vhat := v / (1 - b2)
+				if vhat < 0 {
+					vhat = 0
+				}
+				w.X[l] -= eta * mhat / (math.Sqrt(vhat) + 1e-8)
+			}
+		}
+
+		networks[network].I++
+	}
+	s.I++
+
+	return cost
+}
+
+// Test tests a network
+func (s *System) Test(data []Fisher) {
+	networks := s.Networks
 	histogram := [3][Networks]float64{}
 	for shot := 0; shot < 33; shot++ {
 		for range data {
-			index := rng.Intn(len(data))
-			for s := 0; s < Batch; s++ {
-				transform := MakeRandomTransform(rng, Width, Embedding, Factor)
+			index := s.Rng.Intn(len(data))
+			for b := 0; b < Batch; b++ {
+				transform := MakeRandomTransform(s.Rng, s.Width, s.Embedding, Factor)
 				//offset := MakeRandomTransform(rng, Width, 1, Scale)
 				in := NewMatrix(Width, 1, data[index].Measures...)
 				in = transform.MulT(in) //.Add(offset).Softmax()
 				for n := range networks {
-					copy(networks[n].Others.ByName["input"].X[s*Embedding:(s+1)*Embedding], in.Data)
-					copy(networks[n].Others.ByName["output"].X[s*Width:(s+1)*Width], data[index].Measures)
+					copy(networks[n].Others.ByName["input"].X[b*s.Embedding:(b+1)*s.Embedding], in.Data)
+					copy(networks[n].Others.ByName["output"].X[b*s.Width:(b+1)*s.Width], data[index].Measures)
 				}
 			}
 			losses := make([]Loss, Networks)
@@ -233,7 +261,7 @@ func main() {
 				return losses[i].Loss < losses[j].Loss
 			})
 
-			for _, loss := range losses[:3] {
+			for _, loss := range losses[:s.Subsets] {
 				data[index].Votes[loss.Network]++
 			}
 		}
@@ -248,6 +276,18 @@ func main() {
 		histogram[Labels[item.Label]][index]++
 	}
 	fmt.Println(histogram)
+}
+
+func main() {
+	data := Load()
+	system := NewSystem(1, Networks, 3, Width, Embedding)
+	points := make(plotter.XYs, 0, 8)
+	for i := 0; i < 4*33*len(data); i++ {
+		index := system.Rng.Intn(len(data))
+		cost := system.Learn(data[index].Measures)
+		points = append(points, plotter.XY{X: float64(i), Y: float64(cost)})
+	}
+	system.Test(data)
 
 	p := plot.New()
 
